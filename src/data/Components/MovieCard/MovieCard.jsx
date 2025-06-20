d87f32646b9511c2c8ec38e9ffca361dfdc6dc7e
Joanna Echeverri Porras <jecheverriporras@jecheve-ltmkm4y.internal.salesforce.com>
2025-06-20 13:48:54 -0700

    making images work to deploy

--- src/data/Components/MovieCard/MovieCard.jsx
+++ src/data/Components/MovieCard/MovieCard.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './MovieCard.css';
+import "./MovieCard.css";
+import posterNotAvailable from "src/assets/poster_not_available.png";
 
-function MovieCard({title, imgPath, rating, onClick}) {
-    return (
-        <>
-            <div className="card" onClick={onClick}>
-                {imgPath ? (
-                    <img className="card-img" src={`https://image.tmdb.org/t/p/w500${imgPath}`} alt={title} />
-                ) :(
-                    <img className="card-img" src="src/assets/poster_not_available.png" alt={title} />
-                )}
-                <h2>
-                    {title}
-                </h2>
-                <p className="card-rating">
-                    Rating: {rating}
-                </p>
-            </div>
-        </>
-    );
+function MovieCard({ title, imgPath, rating, onClick }) {
+  return (
+    <>
+      <div className="card" onClick={onClick}>
+        {imgPath ? (
+          <img
+            className="card-img"
+            src={`https://image.tmdb.org/t/p/w500${imgPath}`}
+            alt={title}
+          />
+        ) : (
+          <img className="card-img" src={posterNotAvailable} alt={title} />
+        )}
+        <h2>{title}</h2>
+        <p className="card-rating">Rating: {rating}</p>
+      </div>
+    </>
+  );
 }
 
 export default MovieCard;
